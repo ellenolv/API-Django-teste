3519acb6368f5ceffc9eaa1c2a457c0c7f7a12cb
ellenolv <ellendeoliveirapereira@gmail.com>
2024-10-03 22:52:50 -0300

    Criado - modelos

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+#cria classe chamada user | models (django BD)
+class User(models.Model):
+    
+    # campos que estarão dentro do nosso banco de dados 
+    user_nickname = models.CharField(primary_key=True, max_length=100, default='')
+    user_name = models.CharField(max_length=150, default='')
+    user_email = models.EmailField(default='')
+    user_age = models.IntegerField(default=0)
+
+    #Método mágico: quando chamar qualquer função, ela tbm será chamada
+    def __str__(self):
+        return f'Nickname: {self.user_nickname} | E-mail: {self.user_email}'
+
+
+
+
+
+class UserTasks(models.Model):
+    user_nickname = models.CharField(max_length=100, default='')
+    user_task = models.CharField(max_length=255, default='')
